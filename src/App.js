ae7286863db6bcd09b7839947f174b0d206a0a20
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-06-27 16:55:30 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -11,17 +11,30 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <ul className="App-header">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact Us</Link>
-            </li>
-          </ul>
+          <nav className="grid grid-cols-2 items-center px-[7%] py-4">
+            <div className="">
+              <Link to="/">
+                <a href="/" className="">
+                  <h1 className="uppercase text-xl font-black">
+                    My Cute Kittens
+                  </h1>
+                </a>
+              </Link>
+            </div>
+            <div>
+              <ul className="flex justify-end uppercase font-bold">
+                <li className="">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="ml-7">
+                  <Link to="/about">About Us</Link>
+                </li>
+                <li className="ml-7">
+                  <Link to="/contact">Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
             <Route exact path="/about" element={<About />}></Route>
